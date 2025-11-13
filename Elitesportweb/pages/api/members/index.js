e4150388d2f6f3f9bf534f045cf710dbe5fcc4fe
EPIC\thilina_h <thilina_h@epiclanka.net>
2025-11-13 12:51:53 +0530

    final commit 15

--- Elitesportweb/pages/api/members/index.js
+++ Elitesportweb/pages/api/members/index.js
@@ -3,6 +3,14 @@ import Member from '../../../models/Member'
 import { fallbackMembers } from '../../../lib/fallbackData'
 
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end()
+  }
+
   if (req.method === 'GET') {
     try {
       await dbConnect()
